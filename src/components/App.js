6180f1150696c2b4eef7d64b5dfa92d7368eea9c
Maeva RODRIGUES <maeva.rodrigues@lapiscine.pro>
2024-04-20 20:57:51 +0200

    Add use effect

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Banner from './Banner'
 import logo from '../assets/logo.png'
 import Cart from './Cart'
@@ -7,26 +7,26 @@ import ShoppingList from './ShoppingList'
 import '../styles/Layout.css'
 // import music from '../assets/music.mp3'; // Importer  fichier audio
 
-
-function App() {
-	const [cart, updateCart] = useState([])
-	return (
-		<div>
-			{/* Ajouter musique ici */}
-			 {/* <audio src={music} autoPlay loop style={{ display: 'none' }} /> */}
-
-
-			<Banner>
-				<img src={logo} alt='La maison jungle' className='lmj-logo' />
-				<h1 className='lmj-title'>La maison jungle</h1>
-			</Banner>
-			<div className='lmj-layout-inner'>
-				<Cart cart={cart} updateCart={updateCart} />
-				<ShoppingList cart={cart} updateCart={updateCart} />
-			</div>
-			<Footer />
-		</div>
-	)
-}
-
-export default App
+			 function App() {
+				const savedCart = localStorage.getItem('cart')
+				const [cart, updateCart] = useState(savedCart ? JSON.parse(savedCart) : [])
+				useEffect(() => {
+					localStorage.setItem('cart', JSON.stringify(cart))
+				}, [cart])
+			
+				return (
+					<div>
+						<Banner>
+							<img src={logo} alt='logo-la-maison-jungle' className='lmj-logo' />
+							<h1 className='lmj-title'>La maison jungle</h1>
+						</Banner>
+						<div className='lmj-layout-inner'>
+							<Cart cart={cart} updateCart={updateCart} />
+							<ShoppingList cart={cart} updateCart={updateCart} />
+						</div>
+						<Footer />
+					</div>
+				)
+			}
+			
+			export default App
