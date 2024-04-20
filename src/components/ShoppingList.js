c5d3e0f2608dc125a9c4808b98cc35621f51f9c1
Maeva RODRIGUES <maeva.rodrigues@lapiscine.pro>
2024-04-20 12:50:15 +0200

    Add style on ShoppingList

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,34 +1,30 @@
 import { plantList } from '../datas/plantList'
+import '../styles/ShoppingList.css'
 
 //Component ShoppingList
-// Cette fonction génère une liste de plantes.
 function ShoppingList() {
-	// On extrait les catégories uniques des plantes disponibles
 	const categories = plantList.reduce(
 		(acc, plant) =>
 			acc.includes(plant.category) ? acc : acc.concat(plant.category),
 		[]
 	)
 
-	// Retourne un élément JSX représentant la liste de courses
 	return (
 		<div>
-			{/* Liste des catégories */}
 			<ul>
-				{/* Pour chaque catégorie, on crée un élément de liste */}
 				{categories.map((cat) => (
 					<li key={cat}>{cat}</li>
 				))}
 			</ul>
-			{/* Liste des plantes */}
-			<ul>
-				{/* Pour chaque plante, on crée un élément de liste */}
+			<ul className='lmj-plant-list'>
 				{plantList.map((plant) => (
-					<li key={plant.id}>{plant.name}</li>
+					<li key={plant.id} className='lmj-plant-item'>
+						{plant.name}
+						{plant.isSpecialOffer && <div className='lmj-sales'>Soldes</div>}
+					</li>
 				))}
 			</ul>
 		</div>
 	)
 }
-
 export default ShoppingList
